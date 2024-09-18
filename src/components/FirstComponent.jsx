c6e4cf88d986dc5b066f30e9f0c59521c629b2fd
spartanis119 <camargobarajasdanielfelipe72@gmail.com>
2024-09-17 20:08:35 -0500

    My first Component

---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const FirstComponent = () => {
+  return (
+    <>
+    <div>FirstComponent</div>
+    <h1>Temas de react</h1>
+    <ul>
+        <li>Componentes</li>
+        <li>Eventos</li>
+        <li>Estados/Hooks</li>
+        <li>Props</li>
+    </ul>
+    </>
+  )
+}
